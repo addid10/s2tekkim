8f4260b5a3fbd3c3c5cdafddd6edb9b6a021bb0b
Rezi Rahdianor <rezir008@gmail.com>
2018-10-07 12:48:46 +0800

    Layout

--- admin/layout/sidebar.php
+++ admin/layout/sidebar.php
@@ -8,23 +8,23 @@
                 <nav class="navbar-sidebar">
                     <ul class="list-unstyled navbar__list">
                         <li class="active has-sub">
-                            <a href="#">
+                            <a href="../home">
                                 <i class="fas fa-tachometer-alt"></i>Dashboard</a>
                         </li>
                         <li>
-                            <a href="../home/artikel.php">
+                            <a href="../artikel">
                                 <i class="fas fa-chart-bar"></i>Artikel</a>
                         </li>
                         <li>
-                            <a href="../home/staff.php">
+                            <a href="../staff">
                                 <i class="fas fa-table"></i>Staff</a>
                         </li>
                         <li>
-                            <a href="../home/visimisi.php">
+                            <a href="../visi-misi">
                                 <i class="far fa-check-square"></i>Visi Misi</a>
                         </li>
                         <li>
-                            <a href="../home/tentangkami.php">
+                            <a href="../tentang-kami">
                                 <i class="fas fa-calendar-alt"></i>Tentang Kami</a>
                         </li>
                     </ul>
