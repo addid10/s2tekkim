962aa453fbd3b495a92e8d5d005f18720b352bfe
hani03 <hnpratiwi2@gmail.com>
2018-10-07 15:00:18 +0800

    db ver.2

--- admin/login/login.php
+++ admin/login/login.php
@@ -2,4 +2,11 @@
 require_once('../../koneksi/koneksi.php');
 require('login.view.php');
 
+if(isset($_POST['username']) && isset($_POST['password']){
+
+    $userName = $_POST['username'];
+    $password = $_POST['password']
+
+    var_dump($userName);
+}
 ?>
