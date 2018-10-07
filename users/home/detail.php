08ebc9c96c8534f9f1b8ff38e027f3f616069a21
addid10 <rezkynewaditya@gmail.com>
2018-10-07 19:01:23 +0800

    Template Detail Artikel

---
+++ users/home/detail.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once('../layout/head.php'); ?>
+</head>
+<body>
+<div id="main-wrapper">
+<!-- Page Preloader -->
+<div id="preloader">
+    <div id="status">
+        <div class="status-mes"></div>
+    </div>
+</div>
+
+<div class="uc-mobile-menu-pusher">
+
+<div class="content-wrapper">
+<nav class="navbar m-menu navbar-default navbar-fixed-top detail-navbar">
+    <?php require_once('../layout/navigation.php'); ?>
+    <!-- .container -->
+</nav>
+<!-- .nav -->
+        <div class="row">
+            <div class="mb-40"></div>
+            <section class="about-text ptb-100">
+                <section class="section-title">
+                    <div class="container text-center">
+                        <h2>Judul</h2>
+                        <span class="bordered-icon"><i class="fa fa-circle-thin"></i></span>
+                    </div>
+                </section>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img class="img-responsive" src="../assets/img/img-about.png" alt="">
+                        </div>
+                        <div class="col-md-12">
+                            <p class="contents">Appropriately benchmark bricks-and-clicks e-business before parallel communities. Completely
+                                synergize exceptional outsourcing with principle-centered platforms. Credibly unleash synergistic
+                                internal or "organic" sources without functionalized technology. Objectively brand granular human
+                                capital without goal-oriented e-business. Competently predominate multifunctional web-readiness via
+                                state of the art strategic theme areas.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </section>    
+        <div class="mb-40"></div>               
+        </div>
+
+
+
+<footer class="footer">
+    <?php require_once('../layout/footer.php'); ?>
+</footer>
+<!-- .footer -->
+
+</div>
+<!-- .content-wrapper -->
+</div>
+<!-- .offcanvas-pusher -->
+
+<div class="uc-mobile-menu uc-mobile-menu-effect">
+    <button type="button" class="close" aria-hidden="true" data-toggle="offcanvas"
+            id="uc-mobile-menu-close-btn">&times;</button>
+    <div>
+        <div>
+            <ul id="menu">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="services.html">Services</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- .uc-mobile-menu -->
+
+</div>
+<!-- #main-wrapper -->
+    <?php require_once('../layout/js.php'); ?>
+    <div>
+		<a style="font-size:0; height:0; width:0; opacity:0; position:absolute" target="_blank" href="http://www.uicookies.com">HTML Templates by uiCookies</a>        
+	</div>
+</body>
+</html>
